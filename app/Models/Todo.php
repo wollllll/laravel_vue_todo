a67b7af737040b1acc072e0affc40d583684bc23
kato0406 <sakin50guramu@gmail.com>
2020-10-06 00:57:34 +0900

    ide-helper導入

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,6 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Todo
+ *
+ * @property int $id
+ * @property string $content 内容
+ * @property int $top topの位置
+ * @property int $left leftの位置
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereLeft($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereTop($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Todo whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Todo extends Model
 {
     protected $fillable = [
